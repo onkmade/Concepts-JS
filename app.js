b59fd78ce11a18d8f5f69af83eb49b4f5b9965e5
madesears <solankeadi11@gmail.com>
2025-12-16 02:52:50 +0530

    Updated: 9. Array

--- app.js
+++ app.js
@@ -3,10 +3,14 @@
 const array = ["Mice", "Cat", "Dog"];
 console.log(array); // print array
 console.log(array.length); // Numbers of elements in the array
-console.log(array[array.length - 1]);
+console.log(array[array.length - 1]); // gets the last element of list
 
 array.push("mango");
 console.log(array); // push means add the mongo in the array
 
 array.pop();
 console.log(array); // pop removes the last element by default
+
+array.length = 1;
+console.log(array); // set manual length of an array
+
