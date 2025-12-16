a2d7ce89fcdf5a20fbe1f3ee72b0f0e18b2b3b55
madesears <solankeadi11@gmail.com>
2025-12-16 15:47:48 +0530

    Added 9.1 Looping Through Array

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-// Array
-
-const array = ["Mice", "Cat", "Dog"];
-console.log(array); // print array
-console.log(array.length); // Numbers of elements in the array
-console.log(array[array.length - 1]); // gets the last element of list
-
-array.push("mango");
-console.log(array); // push means add the mongo in the array
-
-array.pop();
-console.log(array); // pop removes the last element by default
-
-array.length = 1;
-console.log(array); // set manual length of an array
-
+let cart = ["apple", "banana"];
+cart.push("orange", "milk");
+console.log(cart);
+cart.pop()
+console.log(cart);
+cart.unshift("bread");
+console.log(cart); // Add in front of the list
