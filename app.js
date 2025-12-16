818a2ad39ac3b8b18f749f8cd3cc16adf1f466e9
madesears <solankeadi11@gmail.com>
2025-12-16 22:31:08 +0530

    Updated: 9.4 Filter method

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const numbers = [5, 10, 15, 20, 25];
+const numbers = [2, 3, 4, 16, 65, 244, 576, 5, 100, 15, 20, 25];
 const doubled = numbers.map( num => num * 2);
-console.log(numbers);
-console.log(doubled);
+console.log(numbers); // original array
+console.log(doubled); // doubled array
+
+const filtering = numbers.filter( num => num % 2 === 0);
+console.log(filtering); // filtered array numbers based on condition
+
+const filter  = numbers.map( num => num % 2 === 0);
+console.log(filter);  // gives true true false false cause num % 2 === 0, always returns the true/false
