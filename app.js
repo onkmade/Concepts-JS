06fdcab9724e21bedfce7c369d64c5a9c4ea65f5
madesears <solankeadi11@gmail.com>
2025-12-16 19:07:59 +0530

    Added 9.3 Map Method

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-let fruits = ["apple", "banana", "orange", "kiwi"];
+const numbers = [5, 12, 8, 20,3];
+let count = 0;
 
-fruits.forEach( (fruit, index) => {
-    console.log(`Index ${index}, Fruits: ${fruit.toUpperCase()}`);
+numbers.forEach( num => {
+    console.log(num*2);
+    if( num > 10){
+        count++;
+    }
 });
+
+
+console.log(`Number greated than 10 are ${count}`);
