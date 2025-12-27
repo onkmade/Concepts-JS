241df1415c156a96fdc461832edc7d2810dccd62
madesears <solankeadi11@gmail.com>
2025-12-27 09:40:05 +0530

    Added Concepts/ String prototype

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-const priceTax = 15120;
-const payTax = 20500;
-const totalTax = priceTax + payTax;
-const profit = totalTax / 100;
-console.log(profit); 
-console.log(profit.toFixed(2)); // rounds the n decimal places
-console.log(profit.toPrecision(3)); // grab the 1st significant bits
+console.log((42).toString());
+co
