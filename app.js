40704bc0dcdb1df6211cb38c14c7dcc84359db4d
madesears <solankeadi11@gmail.com>
2026-01-07 10:39:45 +0530

    Revising: .\Revisiting\ 2.1 Pop Method

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ let two = ["harshu", "nothing", "pride"];
 let result = one.concat(two); // add array "two" into "one"
 console.log(result);
 let result2 = result.concat("Manisha", "Laxmi");
+console.log(result2);
