f1633599bb9e2fce0b5d89cc87c1108fda419997
madesears <solankeadi11@gmail.com>
2026-01-10 16:06:51 +0530

    Revisiting Concepts: 2.2 push(), concat(), and splice()

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-let one = ["something", "news", "aaru"];
-let two = ["harshu", "nothing", "pride"];
-let result = one.concat(two); // add array "two" into "one"
-console.log(result);
-let result2 = result.concat("Manisha", "Laxmi");
-console.log(result2);
+const numb = [1,2,3,4,5,6,7,8,9,10,15];
+const foundd = numb.find( num => num % 2 == 0);
