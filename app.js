0fc5dff8026ad6a4233612eb29a0ed853f11f568
madesears <solankeadi11@gmail.com>
2026-01-13 12:37:30 +0530

    Revisiting Concepts: mental map of logical statements (conditionals) in JavaScript

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-const company = {
-    name: "Blackstone",
-    manufacturing: [ "Food", "Health Care", "Toys", "Shops", "Inventory"], 
-    address:[
-        { street: "AD55 Wall", city: "Kansas", state: "USA", zipcode: 454225 },
-        { street: "Pokemon Mesuem", city: "Paris", state: "France", zipcode: 29955 },
-        { street: "Tower k56", city: "Berlin", state: "Germany", zipcode: 84525 }
-    ], 
-    yearOfEstablishment: 2013
-};
-
-// I want to add the properties to the object company
-company.isOpen = true;
-console.log(company);
-company.address[1].isClose = false;
-
-delete company.isOpen
-console.log(company);
+const myArr3 = [3,6,8,9,3,55,553,434];
+myArr3.sort();
+myArr3.length = 4;
+console.log(myArr3[0]);
